2c5697e7d74f5a974ab97f6b6053cc71e2fc5524
davstore4-del <mamykalume58@gmail.com>
2026-08-16 01:49:55 +0200

    Mise à jour firebase_messaging vers ^16.5.0 (compatibilité cloud_firestore) + import getMessaging préparatoire

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch');
 const { initializeApp, cert, getApps } = require('firebase-admin/app');
 const { getFirestore, FieldValue } = require('firebase-admin/firestore');
 const { getAuth } = require('firebase-admin/auth');
+const { getMessaging } = require('firebase-admin/messaging');
 
 let serviceAccount;
 if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
