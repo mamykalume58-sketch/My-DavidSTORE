0f621b1f5fbe5b8a2e898b1fcb1532970f647bc8
davstore4-del <mamykalume58@gmail.com>
2026-08-16 04:09:17 +0200

    Retrait du chantier Cloudflare Workers (abandonne au profit de Render, carte bancaire bloquante)

--- worker/src/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response(JSON.stringify({ status: 'ok', message: 'DavidSTORE Worker en ligne' }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  },
-};
